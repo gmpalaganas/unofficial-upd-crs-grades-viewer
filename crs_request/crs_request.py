267561b32c15248f4219996a1dbadc13c3eb56f9
Genesis Palaganas <palaganas.genesis@gmail.com>
2015-08-04 13:38:07 +0800

    Move utility functions to crs_request/utils.py

--- crs_request/crs_request.py
+++ crs_request/crs_request.py
@@ -1,3 +1,4 @@
+import utils
 import requests
 import re
 import string
@@ -84,7 +85,7 @@ class CRSHandler:
 
             for row in rows:
                 try:
-                    cols = [ self.format_grade_string(col.string) for col in row.find_all('td')]
+                    cols = [ self.format_grade_string(col.string) for col in row.find_all('td') ]
                     subject = {
                             'tag'        : cols[0],
                             'code'       : cols[1],
@@ -111,48 +112,12 @@ class CRSHandler:
         for grade in grades:
             sem, subjects = grade
             for subject in subjects:
-                if self.is_subject_pass(subject):
-                    unit_count += self.units_string_to_float(subject['units']) 
+                if utils.is_subject_pass(subject):
+                    unit_count += utils.units_string_to_float(subject['units']) 
 
         return unit_count
-
-
-    # Formats the grade string
-    def format_grade_string(self,grade_string):
-        if grade_string == None:
-            return ''
-        else:
-            return to_ascii(grade_string)
     
-    # Converts the units string to float
-    def units_string_to_float(self,units_string):
-        if units_string == None:
-            return 0.0 
-        else:
-            return float(re.sub('\(\d\.\d\)','0.0',to_ascii(units_string)))
-
-    # Converts the grade string into a float 
-    def grade_string_to_float(self,grade_string):
-        grade_string = re.sub('\s+','',grade_string)
-        if grade_string == None:
-            return 0.0
-        else:
-            if re.match(r'(INC\()?\s*P\s*\)?',grade_string):
-                return 3.0
-            elif grade_string == 'INC':
-                return 4.0
-            elif grade_string == 'DRP':
-                return 5.0
-            elif re.match(r'\s+',grade_string):
-                return float(to_ascii(grade_string))
-            else:
-                return 0.0
-    
-    # Checks if the grade garnered in the given class is passing
-    def is_subject_pass(self,subject):
-        grade_float = self.grade_string_to_float(subject['grade'])
-        return grade_float <= 3.0
-
+   
 # An exception meant when the requests fails
 class RequestFailedException(Exception):
     # Constructor takes the response code
@@ -162,6 +127,3 @@ class RequestFailedException(Exception):
     def __str__(self):
         return 'Response code : ' + repr(self.response_code)
 
-# Removes 'tab', 'newline', and unicode characters in the given string
-def to_ascii(s):
-    return re.sub('[\t\n]+','',filter(lambda x: x in string.printable, s))
---
+++ crs_request/utils.py
@@ -0,0 +1,39 @@
+# Removes 'tab', 'newline', and unicode characters in the given string
+def to_ascii(s):
+    return re.sub('[\t\n]+','',filter(lambda x: x in string.printable, s))
+
+# Formats the grade string
+def format_grade_string(grade_string):
+    if grade_string == None:
+        return ''
+    else:
+        return to_ascii(grade_string)
+
+# Converts the units string to float
+def units_string_to_float(units_string):
+    if units_string == None:
+        return 0.0 
+    else:
+        return float(re.sub('\(\d\.\d\)','0.0',to_ascii(units_string)))
+
+# Converts the grade string into a float 
+def grade_string_to_float(grade_string):
+    grade_string = re.sub('\s+','',grade_string)
+    if grade_string == None:
+        return 0.0
+    else:
+        if re.match(r'(INC\()?\s*P\s*\)?',grade_string):
+            return 3.0
+        elif grade_string == 'INC':
+            return 4.0
+        elif grade_string == 'DRP':
+            return 5.0
+        elif re.match(r'\s+',grade_string):
+            return float(to_ascii(grade_string))
+        else:
+            return 0.0
+
+ # Checks if the grade garnered in the given class is passing
+def is_subject_pass(subject):
+    grade_float = utils.grade_string_to_float(subject['grade'])
+    return grade_float <= 3.0
